b2aee2fba6d13ce5723e5d5f47afae5aa699b77e
Guillemot Moussu <guillemot.moussu@gmail.com>
2021-12-06 13:22:19 +0100

    jaaj

--- pendu.py
+++ pendu.py
@@ -1,41 +1,6 @@
-def create_pile(capa):
-    pile=(capa+1)*[None]
-    return pile
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def nombre_elements(pile):
+    nombre_elements_pile=pile[0]
+    return nombre_elements_pile
