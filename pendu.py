3c7f56e162f0fc9823db90cda5866f9771265c21
Guillemot Moussu <guillemot.moussu@gmail.com>
2021-12-06 14:02:08 +0100

    ??

--- pendu.py
+++ pendu.py
@@ -4,19 +4,13 @@ def create_pile(capa):
     pile[0]=0
     return pile
 
-
-
 def nombre_elements(pile):
     return pile[0]
-
-
 
 def empiler(element,pile):
     pile[pile[0]+1]=element
     pile[0]+=1
     return pile
-
-
 
 def depiler(pile):
     element=pile[pile[0]]
@@ -37,6 +31,30 @@ def trier(liste):
     liste_triee=[]
     for index in range(len(liste)):
         if 
+
+
+
+
+
+
+
+
+
+
+
+def killdoublons(liste):
+
+    return liste
+
+
+
+
+
+
+
+
+
+
 
 
 
